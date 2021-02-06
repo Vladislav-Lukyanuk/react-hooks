83a3b52bdec37ba97937a25b84e265b55f05a686
Vladislav-Lukyanuk <44964515+Vladislav-Lukyanuk@users.noreply.github.com>
2021-02-06 22:46:21 +0600

    Update usePrevious.ts

--- usePrevious.ts
+++ usePrevious.ts
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
 
-export const usePrevious = (value: any): any => {
+export const usePrevious = <T>(value: T): T => {
   const ref = useRef();
 
   useEffect(() => {
